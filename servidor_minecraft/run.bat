60fbd641ae9af4682a3adb37f3b51575e33fe0d7
blindjamin1 <bcristobal3199@gmail.com>
2024-06-02 02:37:34 +0000

    Mundo guardado 2024-06-02

--- servidor_minecraft/run.bat
+++ servidor_minecraft/run.bat
@@ -1,10 +1,7 @@
 @echo off
-REM Ejecutar el script de PowerShell
-
-REM Establecer la ruta al script de PowerShell
-set SCRIPT_PATH=%~dp0start.ps1
-
-REM Ejecutar el script de PowerShell
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_PATH%"
-
+REM Forge requires a configured set of both JVM and program arguments.
+REM Add custom JVM arguments to the user_jvm_args.txt
+REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
+REM  pass them to this script directly
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.3.5/win_args.txt %*
 pause
