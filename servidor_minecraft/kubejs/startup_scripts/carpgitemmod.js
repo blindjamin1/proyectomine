31fc3d155b3c7a138bbb967e4ad64f5ca50131b4
Benjamin Paicil <bcristobal3199@gmail.com>
2024-06-01 21:46:41 -0400

    intento final
    vamo shit

---
+++ servidor_minecraft/kubejs/startup_scripts/carpgitemmod.js
@@ -0,0 +1,136 @@
+ItemEvents.modification(event => {
+    event.modify('cataclysm:ignitium_helmet', item => {
+        item.armorProtection = 8
+    })
+    event.modify('minecraft:diamond_helmet', item => {
+        item.armorProtection = 4
+    })
+    event.modify('minecraft:diamond_boots', item => {
+        item.armorProtection = 4
+    })
+    event.modify('minecraft:diamond_chestplate', item => {
+        item.armorProtection = 9
+    })
+    event.modify('minecraft:diamond_leggings', item => {
+        item.armorProtection = 7
+    })
+    event.modify('minecraft:netherite_leggings', item => {
+        item.armorProtection = 8
+    })
+    event.modify('minecraft:netherite_helmet', item => {
+        item.armorProtection = 5
+    })
+    event.modify('minecraft:netherite_chestplate', item => {
+        item.armorProtection = 10
+    })
+    event.modify('minecraft:netherite_boots', item => {
+        item.armorProtection = 5
+    })
+    event.modify('simplyswords:hearthflame', item => {
+        item.attackDamage = 19
+    })
+    event.modify('cataclysm:ignitium_chestplate', item => {
+        item.armorProtection = 12
+    })
+    event.modify('cataclysm:ignitium_elytra_chestplate', item => {
+        item.armorProtection = 13
+    })
+    event.modify('cataclysm:ignitium_leggings', item => {
+        item.armorProtection = 9
+    })
+    event.modify('cataclysm:ignitium_boots', item => {
+        item.armorProtection = 7
+    })
+    event.modify('simplyswords:awakened_lichblade', item => {
+        item.attackDamage = 19
+    })
+    event.modify('simplyswords:sunfire', item => {
+        item.attackDamage = 25
+    })
+    event.modify('simplyswords:harbinger', item => {
+        item.attackDamage = 25
+    })
+    event.modify('simplyswords:livyatan', item => {
+        item.attackDamage = 17
+    })
+    event.modify('simplyswords:shadowsting', item => {
+        item.attackDamage = 11
+    })
+    event.modify('simplyswords:emberblade', item => {
+        item.attackDamage = 14
+    })
+    event.modify('simplyswords:watcher_claymore', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:watching_warglaive', item => {
+        item.attackDamage = 16
+    })
+    event.modify('simplyswords:soulrender', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:molten_edge', item => {
+        item.attackDamage = 16
+    })
+    event.modify('simplyswords:storms_edge', item => {
+        item.attackDamage = 14
+    })
+    event.modify('simplyswords:stormbringer', item => {
+        item.attackDamage = 13
+    })
+    event.modify('simplyswords:mjolnir', item => {
+        item.attackDamage = 15
+    })
+    event.modify('simplyswords:soulkeeper', item => {
+        item.attackDamage = 19
+    })
+    event.modify('simplyswords:arcanethyst', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:soulpyre', item => {
+        item.attackDamage = 17
+    })
+    event.modify('simplyswords:bramblethorn', item => {
+        item.attackDamage = 12
+    })
+    event.modify('simplyswords:toxic_longsword', item => {
+        item.attackDamage = 13
+    })
+    event.modify('celestisynth:aquaflora', item => {
+        item.attackDamage = 12
+    })
+    event.modify('celestisynth:breezebreaker', item => {
+        item.attackDamage = 14
+    })
+    event.modify('celestisynth:crescentia', item => {
+        item.attackDamage = 17
+    })
+    event.modify('celestisynth:solaris', item => {
+        item.attackDamage = 17
+    })
+    event.modify('simplyswords:icewhisper', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:tainted_relic', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:righteous_relic', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:thunderbrand', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:waking_lichblade', item => {
+        item.attackDamage = 16
+    })
+    event.modify('simplyswords:brimstone_claymore', item => {
+        item.attackDamage = 18
+    })
+    event.modify('simplyswords:frostfall', item => {
+        item.attackDamage = 16
+    })
+    event.modify('simplyswords:soulstealer', item => {
+        item.attackDamage = 11
+    })
+    
+    
+})
