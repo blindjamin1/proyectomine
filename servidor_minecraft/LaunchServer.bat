a6e4dff9975b91f3772245c74c8ee1d7947d1a3b
blindjamin1 <bcristobal3199@gmail.com>
2024-06-08 20:29:14 +0000

    Mundo guardado 2024-06-08

---
+++ servidor_minecraft/LaunchServer.bat
@@ -0,0 +1,2 @@
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xmx8000M -Xms1024M -jar forge-1.12.2-14.23.5.2860-universal.jar nogui
+pause
---
+++ servidor_minecraft/LaunchServer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx8G -XX:MaxPermSize=256M -jar forge-1.12.2-14.23.5.2860-universal.jar nogui
